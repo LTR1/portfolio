3a162790651afb6407aa580421ca96d204ae2aed
LTR1 <lrothfork@vcd-ch204-17.eastern.ewu.edu>
2016-11-22 14:31:23 -0800

    portfolio v1
    Addition of plugin “Lightbox.” Addition of img “full-res,” “thumbnails.”

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,18 @@
 // My Scripts
+// Store & Generate Number
+var maxRand = 3;
+var numRand = Math.floor( Math.random() * maxRand );
+//console.log(numRand);
+
+// Store images
+var img0 = '/img/full-res/unsplash.jpg';
+var img1 = '/img/full-res/unsplash-1.jpg';
+var img2 = '/img/full-res/unsplash-2.jpg';
+
+// Show Images
+$('header img').attr('src', eval('img' + numRand) );
+$('figure').lightGallery(
+{
+    selector: 'a'
+    
+});
